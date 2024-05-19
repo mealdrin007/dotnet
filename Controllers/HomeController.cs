cb74d73d60eb0d94f85a61b45fc21d38557a6e09
pocreact <99470886+mealdrin007@users.noreply.github.com>
2024-05-20 00:22:28 +0530

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using evaluationproject.Models;
+
+namespace evaluationproject.Controllers
+{
+    public class HomeController : Controller
+    {
+        evaluationprojectEntities2 db = new evaluationprojectEntities2();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var lst = db.userdetails.ToList();
+            return View(lst);
+        }
+        public ActionResult create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult create(userdetail model)
+        {
+            if (ModelState.IsValid)
+            {
+                db.userdetails.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult Edit(int Id)
+        {
+            userdetail user = db.userdetails.Where(a => a.id == Id).FirstOrDefault();
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult Edit(userdetail model)
+        {
+            userdetail user=db.userdetails.Where(a=>a.id == model.id).FirstOrDefault();
+            user.name = model.name;
+            user.email = model.email;
+            user.password = model.password;
+
+            if (ModelState.IsValid)
+            {
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult Delete(int Id)
+        {
+            userdetail user = db.userdetails.Where(a=>a.id == Id).FirstOrDefault();
+            db.userdetails.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int Id)
+        {
+            userdetail user =db.userdetails.Where(a=>a.id==Id).FirstOrDefault();
+            return View(user);
+        }
+
+    }
+}
---
+++ Controllers/LogInController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+using System.Web.Security;
+using evaluationproject.Models;
+
+namespace evaluationproject.Controllers
+{
+    public class LogInController : Controller
+    {
+        evaluationprojectEntities2 db = new evaluationprojectEntities2();
+        // GET: LogIn
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = db.userdetails.FirstOrDefault(a => a.name == username && a.password == password);
+               if (user != null)
+                {
+                    return RedirectToAction(actionName: "Index", controllerName: "Home");
+                }
+                else
+                {
+                    ViewBag.Message = "Invalid Username or Password";
+                    return View();
+                }
+            }
+            else {
+                return View();
+            }
+        }
+        public ActionResult Logout()
+        {
+            return RedirectToAction("Login", "LogIn");
+        }
+    }
+}
